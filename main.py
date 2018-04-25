19de5e2ba6e419c9cd1e42e2cd966b264caa3367
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-04-25 20:05:01 +0200

    first steps of main logic implemented

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
-    pass
+    from data_handler import DataHandler
+    handler = DataHandler('./resources/small.csv')
+    handler.load_from_file()
     # load data
+
     # split in training and testing
     # train the lin reg model
     # use model to predict on test
     # check correctness
-
