35e263ee560feaeb84bf8cef02e42af122f36be5
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-04-25 18:06:09 +0200

    Template of data handler functionality

---
+++ data_handler/data_handler.py
@@ -0,0 +1,34 @@
+class DataHandler(object):
+    """
+    Class responsible for handling of data used for training and testing models
+    """
+
+    def __init__(self, path_str, delimiter=',', split_ratio = 0.8):
+        _path_str = path_str
+        _delimiter = delimiter
+        _data = [[], []]
+        _train_set = [[], []]
+        _ratio = split_ratio
+
+
+
+
+    def load_from_file(self):
+        """
+        Loads data from csv file and stores it in training and testing sets by ratio specified during initialization.
+        Throws exception if file cannot be open,
+        read or the data is corrupted
+        """
+        pass
+
+    def get_all_data(self):
+        """
+        Returns training and testing sets as a tuple
+        :return: a tuple containing training and testing sets
+        """
+        return (self._train_set, self._test_set)
+
+
+
+
+
