14e5472067345cd6f1e56a5d78e269faae77c071
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-04-25 18:05:23 +0200

    List of steps needed to be addressed

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    pass
+    # load data
+    # split in training and testing
+    # train the lin reg model
+    # use model to predict on test
+    # check correctness
+
