d08e3a94253e36046696d5330ca529581fd337b0
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-04-26 10:59:54 +0200

    Additional tests

--- tests/test_dataHandler.py
+++ tests/test_dataHandler.py
@@ -12,11 +12,15 @@ class TestDataHandler(TestCase):
         self.assertEqual(10, self.dataHandler._size)
         self.assertListEqual([10.0, 20.0, 20.0], self.dataHandler._train_set[0])
         self.assertListEqual([70.0, 30.0, 35.0], self.dataHandler._test_set[0])
+        self.assertEquals(8, len(self.dataHandler._train_set))
+        self.assertEquals(2, len(self.dataHandler._test_set))
 
     def test_split_data(self):
         self.dataHandler.split_data(0.5)
         self.assertListEqual([10.0, 20.0, 20.0], self.dataHandler._train_set[0])
         self.assertListEqual([60.0, 60.0, 60.0], self.dataHandler._test_set[0])
+        self.assertEquals(5, len(self.dataHandler._train_set))
+        self.assertEquals(5, len(self.dataHandler._test_set))
 
     def test_get_train_and_test_sets(self):
         train, test = self.dataHandler.get_train_and_test_sets()
